eef97196649ce492755edd268a93411f28248570
Oleg Afanasiev <oleg.kh81@gmail.com>
2015-12-01 20:26:34 +0200

    Start developing of simulation project

---
+++ bus-traffic-simulation/src/ua/afanasiev/oleg/telesens/trafficsimulation/Main.java
@@ -0,0 +1,10 @@
+package ua.afanasiev.oleg.telesens.trafficsimulation;
+
+/**
+ * Created by oleg on 12/1/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("traffic simulation");
+    }
+}
